0e1c0af8711209a2dcf706e3be0881fbff32922e
James404 <68128416+James404-cyber@users.noreply.github.com>
2021-12-08 19:10:34 +0300

    Add files via upload

---
+++ Fileone.py
@@ -0,0 +1,15 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+   os.system("rm -f .__apikey__.txt")
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from libone import XX_____XXX_____XX
+    XX_____XXX_____XX()
+elif bit == '32bit':
+    from libone import XX_____XXX_____XX
+    XX_____XXX_____XX()
