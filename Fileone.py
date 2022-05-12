662c6523d76a3b50d96a3aa2e13672b431164e97
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-05-12 06:51:32 +0300

    Update Fileone.py

--- Fileone.py
+++ Fileone.py
@@ -6,8 +6,8 @@ try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Jm")._site_ope_()
+	__import__("Jm")._site_view_()
 elif 'aarch' in arc:
-	__import__("Jm64")._site_ope_()
+	__import__("Jm64")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
