657d7141bfa3cd6a6ab87f65d557772c10a91315
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-05-12 06:48:13 +0300

    Update Fileone.py

--- Fileone.py
+++ Fileone.py
@@ -1,9 +1,13 @@
 import platform
-
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Passall")._site_ope_()
+	__import__("Jm")._site_ope_()
 elif 'aarch' in arc:
-	__import__("Passall64")._site_ope_()
+	__import__("Jm64")._site_ope_()
 else:
 	exit(f' Unknow device machine {arc}')
