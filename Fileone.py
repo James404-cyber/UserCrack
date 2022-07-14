633e12b1ef971f06c009f9b64fdd7bee6db5fb4f
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-07-14 12:14:24 +0300

    Update Fileone.py

--- Fileone.py
+++ Fileone.py
@@ -2,14 +2,14 @@ import platform
 import os
 os.system('termux-setup-storage')
 os.system('git pull')
-try:os.system('mkdir /sdcard/ids/OK')
+try:os.system('mkdir /sdcard/OK')
 except:pass
 try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Jm").menu()
+	__import__("Multi").ninex()
 elif 'aarch' in arc:
-	__import__("Passall").menu()
+	__import__("Multi").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
