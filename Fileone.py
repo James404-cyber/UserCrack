c9d7e3c3e7c83ae3757b66032402db35542a11bb
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-08-03 07:42:30 +0300

    Update Fileone.py

--- Fileone.py
+++ Fileone.py
@@ -8,8 +8,8 @@ try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Multi").ninex()
+	__import__("User").ninex()
 elif 'aarch' in arc:
-	__import__("Multi").ninex()
+	__import__("User").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
